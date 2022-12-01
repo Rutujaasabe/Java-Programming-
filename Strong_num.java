cab5539f60f4a946b82e2d1762d35104ef77df8f
Rutujaasabe <97525707+Rutujaasabe@users.noreply.github.com>
2022-12-01 21:06:22 +0530

    Add files via upload

---
+++ Strong_num.java
@@ -0,0 +1,35 @@
+import java . util. Scanner;
+ class Strong_num
+ {
+         public static void main(String rutu[])
+   {
+         int n,i;
+         int fact,rem;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the number : ");
+       n = sc.nextInt();
+       int sum = 0;
+        int temp = n;
+   while(n != 0)
+{
+   i = 1;
+   fact =1;
+   rem = n % 10 ;
+
+  while(i <= rem)
+{
+   fact = fact * i;
+    i++;
+}
+    sum = sum + fact;
+    n = n / 10;
+}
+
+  if(sum == temp)
+         System.out.println(temp + " is a strong number\n");
+  else
+         System.out.println(temp + " is not a strong number\n");
+
+System.out.println();
+   }
+}
