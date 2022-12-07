a6c681b540cbd69520bc9c62fa0338af87121ecd
Rutujaasabe <97525707+Rutujaasabe@users.noreply.github.com>
2022-12-07 23:50:53 +0530

    Add files via upload

---
+++ Prime_num_sum.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Prime_num_sum
+{
+   public static void main(String[] args)
+  {
+     Scanner sc = new Scanner(System.in);
+     System.out.print("Enter a number : ");				
+     int number = sc.nextInt();
+     int x = 0;
+	for(int i = 2 ; i <= number/2 ; i++)
+		
+{
+               if(prime_or_not(i) == 1)
+     {
+	if(prime_or_not(number-i) == 1)
+      {
+					
+                System.out.println(number+ " = "+i+" + "+(number-i));
+	x = 1;
+			
+}
+       }
+    }
+            if(x == 0)
+	System.out.println(+number+" cannot be expressed as a sum of two prime numbers");
+      }
+      
+       public static int prime_or_not(int n)
+{
+      int c = 1;
+          for(int i = 2 ; i < n ; i++)
+      {
+          if(n % i == 0)
+     {
+	c=0;
+	break ;			
+}
+}
+       return c;
+
+	}
+}
